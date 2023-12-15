bf4ad7b10341a2ce8a53c8f997c6d68d401211ee
Talat09 <talat181008312@gmail.com>
2023-12-16 00:08:41 +0600

    done project setup and install required packeges

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl md:text-5xl text-center text-sky-400 mt-8 font-semibold">
+        Welcome to React <span className="text-sky-800  ">Redux Thunk</span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
